34b90e089f321b6b473cfac5b11f9208a188d7ff
Michael C <cataldmj194@potsdam.edu>
2018-02-20 11:06:11 -0500

    Updated Network_IDS.java. Now uses Apache Spark's Streaming libraries to connect to port 9999 on localhost as a proof of concept for live packet capture. Also parses kddcup.data_10_percent into a Dataset of Strings as another proof of concept of how to parse & use the dataset. Also parsed The dataset into lines and then rows to pass into a StreamingQuery to familiarize self with StreamingQueries. Followed simple wordcount program, tries to parse kddcup data into separate lines and then count number of unique entries. However I run into an error when spark parses job 3, I run out of memory

--- src/main/java/Network_IDS.java
+++ src/main/java/Network_IDS.java
@@ -4,14 +4,46 @@
  *           Machine Learning (in particular, k-means clustering) 
  *           and Streaming Libraries.
  **/
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+import java.util.Iterator;
+import java.util.Arrays;
 
 public class Network_IDS {
-  public static void main(String[] args){
-    SparkSession spark = 
-                 SparkSession.builder().appName("NIDS").getOrCreate();
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws org.apache.spark
+    .sql.streaming.StreamingQueryException {
+
+    String kddcup = "src/main/resources/kddcup99/kddcup.data_10_percent";
+    SparkSession spark = SparkSession
+      .builder()
+      .appName("NIDS")
+      .getOrCreate();
+
+    Dataset<String> kddData = spark.read().textFile(kddcup).cache();  
+
+    Dataset<Row> lines = spark
+      .readStream().format("socket")
+      .option("host","localhost")
+      .option("port", 9999)
+      .load();
+
+    Dataset<String> words = lines
+      .as(Encoders.STRING())
+      .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+
+    Dataset<Row> wordCounts = lines.groupBy("value").count();
+    StreamingQuery query = wordCounts.writeStream()
+      .outputMode("complete")
+      .format("console")
+      .start();
+
+    query.awaitTermination();
+
     spark.stop();
   }
 }
